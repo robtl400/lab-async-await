e79816bbac069aab253b45e15ff51555d92736a1
Rob Lord <robertlord@outlook.com>
2025-11-22 00:23:52 -0800

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your code here!
+
+async function fetchAndDisplayPosts() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const posts = await response.json();
+  displayPosts(posts);
+}
+
+
+function displayPosts(posts) {
+  const postList = document.getElementById('post-list');
+
+  for (let i = 0; i < posts.length; i++) {
+    const li = document.createElement('li');
+    const h1 = document.createElement('h1');
+    h1.textContent = posts[i].title;
+    
+    const p = document.createElement('p');
+    p.textContent = posts[i].body;
+    
+    li.appendChild(h1);
+    li.appendChild(p);
+
+    postList.appendChild(li);
+  }
+}
+
+fetchAndDisplayPosts();
